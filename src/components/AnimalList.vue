d80c9bc15eb9ec2caed1bbb3f9adf26eda65965b
Sinisa Manojlovic <sinisamanojlovic1@gmail.com>
2018-11-20 18:42:57 +0100

    Zadatak 2

--- src/components/AnimalList.vue
+++ src/components/AnimalList.vue
@@ -11,7 +11,9 @@
                 <tr v-for="(animal, index) in animals" :key="index">
                     <td>{{animal.spicies}}</td>
                     <td>{{animal.name}}</td>
-                    <td>{{animal.dateOfBirth}}</td>
+                    <td v-if="animal.dateOfBirth !== ''">{{animal.dateOfBirth}}</td>
+                    <td v-if="animal.dateOfBirth === ''">'Nepoznat'</td>
+                    
                 </tr>
             </tbody>
         </table>
@@ -26,9 +28,11 @@ export default {
                 {spicies: 'Sisar', name: 'Spajk', dateOfBirth: '10 05 2006'},
                 {spicies: 'Glodar', name: 'Misko', dateOfBirth: '11 06 2007'},
                 {spicies: 'Vodozemac', name: 'Zabra', dateOfBirth: '12 07 2007'},
-                {spicies: 'Dinosaurus', name: 'Spajk', dateOfBirth: '10 05 2006'},
-                {spicies: 'Sisar', name: 'Sinisa', dateOfBirth: '16 08 2016'}
+                {spicies: 'Dinosaurus', name: 'Perodaktil', dateOfBirth: '10 05 2006'},
+                {spicies: 'Sisar', name: 'Sinisa', dateOfBirth: '16 08 2016'},
+                {spicies: 'Komsija', name: 'Dejan', dateOfBirth: ''}
             ]
+            
         }
 
     },
